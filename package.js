034f92094ab7fc526dffd456f9898c1c4d8dc56d
David Collado <bitomule@gmail.com>
2014-08-25 12:41:18 +0200

    Right version

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: 'Simple server method that finds images, page name and description from an url',
-  version: '0.1.2'
+  version: '0.1.3'
 });
 
 Package.on_use(function (api) {
