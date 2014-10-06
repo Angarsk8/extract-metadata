ee7c3ae97f9ace1d7e627dd9c2ac4f2466900f5e
David Collado <bitomule@gmail.com>
2014-10-06 10:29:06 +0200

    Added required packages

--- package.js
+++ package.js
@@ -1,12 +1,14 @@
 Package.describe({
   summary: 'Simple server method that finds images, page name and description from an url',
-  version: '0.2.0',
+  version: '0.2.1',
   git: 'https://github.com/bitomule/simple-metacrawler.git',
   name: "bitomule:simple-metacrawler"
 });
 
 Npm.depends({
-    'cheerio': '0.17.0'
+    'cheerio': '0.17.0',
+    'request': '2.45.0',
+    'iconv': '2.1.4'
 });
 
 Package.on_use(function (api) {
