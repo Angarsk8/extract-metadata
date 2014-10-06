f132e1fb772a82e1bb445de3a18a3b05d99fd0eb
David Collado <bitomule@gmail.com>
2014-10-06 09:37:51 +0200

    Npm dependency fix

--- package.js
+++ package.js
@@ -1,8 +1,13 @@
 Package.describe({
   summary: 'Simple server method that finds images, page name and description from an url',
-  version: '0.1.8',
+  version: '0.1.9',
   git: 'https://github.com/bitomule/simple-metacrawler.git',
   name: "bitomule:simple-metacrawler"
+});
+
+Npm.depends({
+    'cheerio': '0.17.0',
+    'request': '2.44.0'
 });
 
 Package.on_use(function (api) {
@@ -10,9 +15,6 @@ Package.on_use(function (api) {
 	if(api.versionsFrom) {
 		api.versionsFrom('METEOR@0.9.0');
 	}
-
-	Npm.depends({cheerio: '0.17.0',request: '2.44.0'});
-
 	api.add_files('server/crawler.js', ['server']);
 
 });
