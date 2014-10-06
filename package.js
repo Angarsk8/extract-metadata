bf730b13b6c698adacced09c45dc6f1726608f00
David Collado <bitomule@gmail.com>
2014-10-06 09:50:40 +0200

    Removed request npm dependency

--- package.js
+++ package.js
@@ -1,13 +1,12 @@
 Package.describe({
   summary: 'Simple server method that finds images, page name and description from an url',
-  version: '0.1.9',
+  version: '0.2.0',
   git: 'https://github.com/bitomule/simple-metacrawler.git',
   name: "bitomule:simple-metacrawler"
 });
 
 Npm.depends({
-    'cheerio': '0.17.0',
-    'request': '2.44.0'
+    'cheerio': '0.17.0'
 });
 
 Package.on_use(function (api) {
